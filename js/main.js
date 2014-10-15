1ea48f10f14d95df2887573757358efbfe9cb466
Ezekiel Mangalu <emangalu@gmail.com>
2014-10-15 08:45:11 +0800

    Moved the Js to jQuery

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-init();
+$(function() {
+	function init()
+	{
+		toggleMenu('menu', 'site-wrap', 'nav');
+	}
 
-function init()
-{
-	toggleMenu('menu', 'site-wrap', 'nav');
-}
+	// Toggle menu
+	function toggleMenu(menuId, siteWrapClass, navId) {
+		var menu = $('#' + menuId),
+			menuIcon = menu.find('.icon*'),
+			nav = $('#' + navId),
+			bodyWrap = $('.' + siteWrapClass);
 
-// Toggle menu
-function toggleMenu(menuId, siteWrapClass, navId) {
-	var menu = document.getElementById(menuId),
-		menuIcon = menu.childNodes[0],
-		nav = document.getElementById(navId),
-		bodyWrap = document.getElementsByClassName(siteWrapClass)[0];
+		// Hide nav
+		nav.hide();
 
-	// Hide nav
-	nav.style.display = 'none';
+		// On click of menu
+		menu.click(function() {
+			if(nav.css('display') === 'none')
+			{
+				nav.show('slow');
+			} else {
+				nav.hide('slow');
+			}
 
-	// On click of menu
-	menu.onclick = function() {
-		if (nav.style.display == 'none')
-		{
-			// Show nav
-			nav.style.display = '';
-			menuIcon.className = 'icon-close'; // Change menu icon
-		}
-		else
-		{
-			// Close nav
-			nav.style.display = 'none';
-			menuIcon.className = 'icon-menu';
-		}
-	};
+			menuIcon.toggleClass('icon-close', 'icon-menu'); // Change menu icon
+		});
+	}
 
-}
+	// Run stuff
+	init();
+
+});
+
